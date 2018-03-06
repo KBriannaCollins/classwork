3cd7bdb9d5aeac2b04d476bf27ac8b2253ee71cf
Kearsten Collins <k.brianna.collins@gmail.com>
2018-03-06 10:14:36 -0600

    Trying This Out

---
+++ cs265/PlayingAround/PlayingAround.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string yep;
+
+	cout << "Enter the sentence you want to print.\n";
+	getline(cin, yep);
+	cout << yep;
+
+	return o;
+}
